eab3fb93c8d415465167135c500140e493d0b1ad
Hriday Keswani <hridaykeswani@gmail.com>
2022-07-15 12:36:53 +0530

    implement fragment navigation

--- app/src/main/java/com/example/implementfragment/MainActivity.kt
+++ app/src/main/java/com/example/implementfragment/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.implementfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.button1)
+        val btn2 = findViewById<Button>(R.id.button2)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragmentOne())
+            commit()
+        }
+
+        btn1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, fragmentOne())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btn2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, fragmentTwo())
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
